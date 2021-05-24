d28829d95cb18626fe1c663cbdb702734c357676
Nathan Jeynes <nathan@medialogix.co.uk>
2021-05-24 21:34:12 +0100

    even smaller

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ input.onButtonPressed(Button.A, function () {
         basic.showIcon(IconNames.No)
         music.stopAllSounds()
         pause(2000)
-        setUp()
+setUp()
     }
 })
 function runFunction1 () {
@@ -47,55 +47,41 @@ function runFunction3 () {
     while (runningSequence) {
         dS2 = Kitronik_Move_Motor.measure()
         if (dS2 >= 5) {
-            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 40)
             rnd4 = Math.floor(Math.random() * 4)
-            if (rnd4 <= 1) {
-                // go forward?
-                fRnd = Math.floor(Math.random() * 20)
-                for (let index = 0; index < fRnd; index++) {
-                    if (dS2 > 5) {
-                        if (rnd4 == 0) {
-                            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 40)
-                        } else {
-                            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 40)
-                        }
-                        pause(100);
-dS2 = Kitronik_Move_Motor.measure()
-                    }
-                }
-            }
-            if (rnd4 >= 2) {
-                // spin left - up to 2 sec
-                if (rnd4 == 2) {
-                    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 40)
-                } else {
-                    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 40)
-                }
+            Kitronik_Move_Motor.stop()
+            basic.showNumber(rnd4)
+            if (rnd4 == 0) {
+                Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 60)
+                pause(1000);
+            } else if (rnd4 == 1) {
+                Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 60)
+                pause(1000);
+            } else if (rnd4 == 2) {
+                Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 80)
                 rndLS = Math.floor(Math.random() * (2000 - 100)) + 100
                 pause(rndLS)
-Kitronik_Move_Motor.stop()
+            } else if (rnd4 == 3) {
+                Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 80)
+                rndLS = Math.floor(Math.random() * (2000 - 100)) + 100
+                pause(rndLS)
             }
         } else {
             Kitronik_Move_Motor.stop()
-            // lets go random
-            rnd4 = Math.floor(Math.random() * 2)
-            // Should change direction.Go back first a bit! - go back 30
-            while (dS2 <= 15) {
-                Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 40)
-                dS2 = Kitronik_Move_Motor.measure()
-            }
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 80)
+            pause(500);
+            
         }
     }
 }
 input.onButtonPressed(Button.B, function () {
     if (runningSequence == false && currentNumber > 0) {
-        basic.showIcon(IconNames.Yes)
+        
         if (currentNumber == 1) {
             music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Once)
             pause(5000);
 runFunction1()
         } else if (currentNumber == 2) {
-            music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+            //music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
             basic.pause(5000)
             runFunction2()
         } else if (currentNumber == 3) {
@@ -135,26 +121,21 @@ function runFunction2 () {
                 Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 40)
                 distance = Kitronik_Move_Motor.measure()
             }
-            
-                // go left
-                if (random == 0) {
+            // go left
+            if (random == 0) {
                 Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 40)
-                }  else  {
+            } else {
                 // go right
                 Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 40)
-                }
-                rnd2 = Math.floor(Math.random() * (1000 - 300)) + 300
-                pause(rnd2)
+            }
+            rnd2 = Math.floor(Math.random() * (1000 - 300)) + 300
+            pause(rnd2)
 Kitronik_Move_Motor.stop()
-           
-                
-            
             allLights.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Purple))
         }
     }
 }
 let distance = 0
-let fRnd = 0
 let rnd4 = 0
 let dS2 = 0
 let lDiff = 0
@@ -162,13 +143,14 @@ let rLS = 0
 let lLS = 0
 let runningSequence = false
 let currentNumber = 0
-let allLights:  Kitronik_Move_Motor.MoveMotorZIP = null
-let rnd3 = 0
-let rnd2 = 0
-let random = 0
-let bckRnd = 0
-let rndLS = 0
+let allLights: Kitronik_Move_Motor.MoveMotorZIP = null
 let rndRS = 0
+let rndLS = 0
+let bckRnd = 0
+let random = 0
+let rnd2 = 0
+let rnd3 = 0
+let fRnd = 0
 let mMZ = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
 allLights = mMZ.range(0, 4)
 setUp()
